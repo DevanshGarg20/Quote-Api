7c3c1132399c4b46bed567a22b2f82254c707c9d
Devansh Garg <dg.devansh.dg@gmail.com>
2021-02-14 02:28:29 +0530

    added get route for random, all, individual quotes

--- server.js
+++ server.js
@@ -8,3 +8,23 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes', (req,res,next) => {
+  if(!req.query.person){
+    res.send({quotes: quotes})
+  }else{
+    const temp = quotes.filter((quote) => {
+      return quote.person === req.query.person
+    })
+    res.send({quotes: temp})
+    }
+  }
+)
+
+app.get('/api/quotes/random', (req,res,next) => {
+  const randomElement = getRandomElement(quotes)
+  res.send({quote: randomElement})
+})
+
+app.listen(PORT, () => {
+  console.log("server is up and running");
+})
